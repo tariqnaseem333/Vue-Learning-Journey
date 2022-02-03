c39436b74673d44cab67d541bb7e49b36bd18b04
Tariq Naseem <tariqnaseem333@gmail.com>
2022-02-03 15:57:24 +0530

    Added some comments

--- Vue Basics/app.js
+++ Vue Basics/app.js
@@ -2,14 +2,16 @@ console.log("Hello Vue")
 
 // Creating a Vue App
 const app = Vue.createApp({
+    // {} -- Root Component of Vue app
     // setup data functions
+    // the template below will be injected inside the element 
     // template: "<h2>It's a template</h2>"
     data() {
         // We are returning a data object here
         return  {
-            title: "The Final Empire",
-            author: "Brandon",
-            age: 45
+            title: "A History of Internet",
+            author: "John Ryan",
+            age: 88
         }
     },
     methods: {
@@ -20,4 +22,6 @@ const app = Vue.createApp({
 })
 
 // Telling where to mount this app in DOM (an element having id of app)
+// which means this app exist for this element and all functions inside this 
+// app will be available only inside this element
 app.mount("#app")
