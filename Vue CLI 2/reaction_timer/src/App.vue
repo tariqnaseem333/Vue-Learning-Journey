493225748002048feae65d388d460786c731f45e
Tariq Naseem <tariqnaseem333@gmail.com>
2022-02-05 18:12:40 +0530

    Created Reaction Timer Project

---
+++ Vue CLI 2/reaction_timer/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+
+export default {
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+    endGame(reactionTime) {},
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
